2c67e6ec2f07651590a2656b0a39f9142b959d7d
RebarFdn <118987883+RebarFdn@users.noreply.github.com>
2025-05-01 09:09:46 -0500

    First push

---
+++ pyform/config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from starlette.templating import Jinja2Templates
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+## This is the configuration file for the pyform project.
+## It sets up the base path, static files path, and templates path.
+
+BASE_PATH = Path(__file__).resolve().parent
+STATIC_PATH = BASE_PATH / "static"
+TEMPLATES_PATH = BASE_PATH / "templates"
+
+#Network configuration
+NETWORK_CONFIG = {
+    "host": "0.0.0.0",
+    "port": 9093,
+    "debug": True,
+}
+
+# Template configuration
+
+# Template settings
+TEMPLATES = Jinja2Templates(directory=TEMPLATES_PATH)
+
---
+++ pyform/server.py
@@ -0,0 +1,51 @@
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette_wtf import CSRFProtectMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.routing import Route, Mount
+from starlette.staticfiles import StaticFiles
+#from routes import router
+from config import STATIC_PATH, Path, NETWORK_CONFIG, TEMPLATES
+
+async def homepage(request):
+    return TEMPLATES.TemplateResponse("index.html", {"request": request})
+
+
+async def form(request):
+    return TEMPLATES.TemplateResponse("daisy.html", {"request": request})
+
+router = [
+    Route("/", homepage),
+    Route("/form", form),
+    Mount("/static", StaticFiles(directory=STATIC_PATH), name="static"),
+    ]  # Placeholder for the router, replace with actual routes
+
+
+app = Starlette(
+    debug=NETWORK_CONFIG.get('debug'),
+    routes=router,
+    middleware=[
+        Middleware(SessionMiddleware, secret_key="!secret"),
+        Middleware(CSRFProtectMiddleware, csrf_secret="!secret"),
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_methods=["GET", "POST", "PUT", "DELETE"],
+            allow_headers=["*"],
+            allow_credentials=True,
+        ),
+    ],
+    
+    )
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    key_path:Path = None #STATIC_PATH /  "localhost+2-key.pem"
+    cert_path:Path = None # STATIC_PATH / "localhost+2.pem"
+    try:
+        uvicorn.run(app=app, host=NETWORK_CONFIG.get('host'), port=NETWORK_CONFIG.get('port'), ssl_certfile=cert_path, ssl_keyfile=key_path )
+    except Exception as e:
+        print(str(e))
